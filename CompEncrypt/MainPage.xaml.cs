d10e3b4ccf543030eaf710418bf230f7b86fe562
Paul Johnson <Paul.Johnson@hicom.co.uk>
2022-04-29 15:35:09 +0100

    add animation code

--- CompEncrypt/MainPage.xaml.cs
+++ CompEncrypt/MainPage.xaml.cs
@@ -5,6 +5,31 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+        ViewModel.Init();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+    }
+
+    async void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == "IsBusy")
+        {
+            if (ViewModel.IsBusy)
+            {
+                await LabelLoad.RotateTo(360, 1000, Easing.Linear);
+                LabelLoad.Rotation = 0;
+            }
+        }
+    }
 }
 
